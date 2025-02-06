be41f64bb372651034c797d824b0cd75209134bc
JMarrink <jensmarrink@gmail.com>
2025-02-06 23:44:50 +0100

    added paragraph

--- index.php
+++ index.php
@@ -99,96 +99,138 @@
             </div>
         </div>
     </section>
-    </header>
+</header>
     <main>
-    <section class="hero">
-        <div class="container">
-            <div class="hero__content">
-                <h1 class="hero__title">WatersportAlmanak</h1>
-                <h2 class="hero__subtitle">Dé gids voor alle watersporters</h2>
-                <p class="hero__description">
-                    WatersportAlmanak is de grootste informatiebron voor watersporters in Nederland. Op de website vindt u zeer veel informatie over varen in het algemeen, vaarregels, jachthavens, vaarroutes en natuurlijk de getijdentabellen van geheel Nederland.
-                </p>
-                <div class="hero__buttons">
-                    <a class="button button-primary" href="">Watersport winkel</a>
-                    <a class="button button-outline-light" href="">Getijden app</a>
+        <section class="hero">
+            <div class="container">
+                <div class="hero__content">
+                    <h1 class="hero__title">WatersportAlmanak</h1>
+                    <h2 class="hero__subtitle">Dé gids voor alle watersporters</h2>
+                    <p class="hero__description">
+                        WatersportAlmanak is de grootste informatiebron voor watersporters in Nederland. Op de website vindt u zeer veel informatie over varen in het algemeen, vaarregels, jachthavens, vaarroutes en natuurlijk de getijdentabellen van geheel Nederland.
+                    </p>
+                    <div class="hero__buttons">
+                        <a class="button button-primary" href="">Watersport winkel</a>
+                        <a class="button button-outline-light" href="">Getijden app</a>
+                    </div>
                 </div>
-            </div>
-            <div class="callout-cards">
-                <!-- Slider main container -->
-                <div class="swiper" id="callout-swiper">
-                <!-- Additional required wrapper -->
-                    <div class="swiper-wrapper">
-                        <!-- Slides -->
-                        <div class="swiper-slide card">
-                            <h4 class="card__title">Watersport winkel</h4>
-                            <p class="card__description">
-                                Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
-                            </p>
-                            <a href="" class="card__link">
-                                Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
-                            </a>
-                        </div>
-                        <div class="swiper-slide card">
-                            <h4 class="card__title">Watersport winkel</h4>
-                            <p class="card__description">
-                                Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
-                            </p>
-                            <a href="" class="card__link">
-                                Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
-                            </a>
-                        </div>
-                        <div class="swiper-slide card">
-                            <h4 class="card__title">Watersport winkel</h4>
-                            <p class="card__description">
-                                Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
-                            </p>
-                            <a href="" class="card__link">
-                                Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
-                            </a>
-                        </div>
-                        <div class="swiper-slide card">
-                            <h4 class="card__title">Watersport winkel</h4>
-                            <p class="card__description">
-                                Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
-                            </p>
-                            <a href="" class="card__link">
-                                Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
-                            </a>
-                        </div>
-                        <div class="swiper-slide card">
-                            <h4 class="card__title">Watersport winkel</h4>
-                            <p class="card__description">
-                                Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
-                            </p>
-                            <a href="" class="card__link">
-                                Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
-                            </a>
-                        </div>
-                        <div class="swiper-slide card">
-                            <h4 class="card__title">Watersport winkel</h4>
-                            <p class="card__description">
-                                Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
-                            </p>
-                            <a href="" class="card__link">
-                                Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
-                            </a>
+                <div class="callout-cards">
+                    <!-- Slider main container -->
+                    <div class="swiper" id="callout-swiper">
+                    <div class="swiper__controls">
+                        <div class="swiper__controls-prev"><i class="fa-solid fa-arrow-left"></i></div>
+                        <div class="swiper__controls-next"><i class="fa-solid fa-arrow-right"></i></div>
+                    </div>
+                    <!-- Additional required wrapper -->
+                        <div class="swiper-wrapper">
+                            <!-- Slides -->
+                            <div class="swiper-slide card">
+                                <h4 class="card__title">Watersport winkel</h4>
+                                <p class="card__description">
+                                    Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
+                                </p>
+                                <a href="" class="card__link">
+                                    Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
+                                </a>
+                            </div>
+                            <div class="swiper-slide card">
+                                <h4 class="card__title">Watersport winkel</h4>
+                                <p class="card__description">
+                                    Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
+                                </p>
+                                <a href="" class="card__link">
+                                    Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
+                                </a>
+                            </div>
+                            <div class="swiper-slide card">
+                                <h4 class="card__title">Watersport winkel</h4>
+                                <p class="card__description">
+                                    Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
+                                </p>
+                                <a href="" class="card__link">
+                                    Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
+                                </a>
+                            </div>
+                            <div class="swiper-slide card">
+                                <h4 class="card__title">Watersport winkel</h4>
+                                <p class="card__description">
+                                    Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
+                                </p>
+                                <a href="" class="card__link">
+                                    Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
+                                </a>
+                            </div>
+                            <div class="swiper-slide card">
+                                <h4 class="card__title">Watersport winkel</h4>
+                                <p class="card__description">
+                                    Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
+                                </p>
+                                <a href="" class="card__link">
+                                    Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
+                                </a>
+                            </div>
+                            <div class="swiper-slide card">
+                                <h4 class="card__title">Watersport winkel</h4>
+                                <p class="card__description">
+                                    Wij leveren uitsluitend watersportartikelen waarvan we zelf vinden dat ze aan onze kwaliteitseisen voldoen
+                                </p>
+                                <a href="" class="card__link">
+                                    Bekijk het aanbod <i class="fa-solid fa-arrow-right card__icon"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <section class="paragraph paragraph--img paragraph--img-left">
+            <div class="container">
+                <div class="paragraph__wrapper">
+                    <div class="paragraph__content">
+                        <h2 class="paragraph__title">Getijden app</h2>
+                        <div class="paragraph__description">
+                            <p>De Getijden App biedt een schat aan informatie over de getijden op locaties over de hele wereld. Of je nu een ervaren zeeman bent of gewoon geïnteresseerd bent in de natuurlijke ritmes van de zee, deze app heeft voor ieder wat wils. Hier zijn enkele opmerkelijke kenmerken van deze app:</p>
+                            <p>1. Nauwkeurige Getijdeninformatie: De app haalt real-time getijdeninformatie op voor duizenden locaties wereldwijd, zodat je altijd op de hoogte bent van de actuele getijdenstanden, zowel vooruit als achteruit in de tijd.</p>
+                            <p>2. Gedetailleerde Grafieken: Grafische weergaven van getijdenpatronen en voorspellingen helpen je om de eb en vloed op een visuele manier te begrijpen. Hierdoor kun je gemakkelijk plannen wanneer het beste moment is om te gaan vissen, surfen of gewoon te genieten van het strand.</p>
+                            <p>3. Notificaties: Stel aangepaste meldingen in, zodat je op de hoogte wordt gebracht wanneer er belangrijke veranderingen in de getijden optreden. Dit is handig voor mensen die afhankelijk zijn van het getij bij hun dagelijkse activiteiten.</p>
+                        </div>
+                    </div>
+                    <div class="paragraph__img">
+                        <img src="/assets/images/app-img.webp" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="paragraph paragraph--img paragraph--img-right" style="background-color: #F9F9FB;">
+            <div class="container">
+                <div class="paragraph__wrapper">
+                    <div class="paragraph__content">
+                        <h2 class="paragraph__title">Getijden app</h2>
+                        <div class="paragraph__description">
+                            <p>De Getijden App biedt een schat aan informatie over de getijden op locaties over de hele wereld. Of je nu een ervaren zeeman bent of gewoon geïnteresseerd bent in de natuurlijke ritmes van de zee, deze app heeft voor ieder wat wils. Hier zijn enkele opmerkelijke kenmerken van deze app:</p>
+                            <p>1. Nauwkeurige Getijdeninformatie: De app haalt real-time getijdeninformatie op voor duizenden locaties wereldwijd, zodat je altijd op de hoogte bent van de actuele getijdenstanden, zowel vooruit als achteruit in de tijd.</p>
+                            <p>2. Gedetailleerde Grafieken: Grafische weergaven van getijdenpatronen en voorspellingen helpen je om de eb en vloed op een visuele manier te begrijpen. Hierdoor kun je gemakkelijk plannen wanneer het beste moment is om te gaan vissen, surfen of gewoon te genieten van het strand.</p>
+                            <p>3. Notificaties: Stel aangepaste meldingen in, zodat je op de hoogte wordt gebracht wanneer er belangrijke veranderingen in de getijden optreden. Dit is handig voor mensen die afhankelijk zijn van het getij bij hun dagelijkse activiteiten.</p>
+                        </div>
+                    </div>
+                    <div class="paragraph__img">
+                        <img src="/assets/images/app-img.webp" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
         const heroSwiper = new Swiper('#callout-swiper', {
             slidesPerView: "auto", // Zorgt ervoor dat slides hun natuurlijke breedte behouden
-      spaceBetween: 60, // Ruimte tussen de slides
-      centeredSlides: false, // Voorkomt dat de slides gecentreerd worden
-    //   freeMode: true, // Zorgt voor natuurlijk scrollen
-    //   watchOverflow: true, // Voorkomt dat Swiper scrollbaar is als er te weinig slides zijn
+            spaceBetween: 60, // Ruimte tussen de slides
+            centeredSlides: false, // Voorkomt dat de slides gecentreerd worden
+            navigation: {
+                nextEl: ".swiper__controls-next",
+                prevEl: ".swiper__controls-prev",
+            },
         });
     </script>
     <script>
